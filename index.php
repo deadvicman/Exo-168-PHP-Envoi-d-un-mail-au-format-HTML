898aacfdf9d231f10460836f77a33d3fa836d8b3
victorien <deadvicmangame@gmail.com>
2023-01-20 13:18:26 +0100

    exo 168 envoi dun mail au format html

--- index.php
+++ index.php
@@ -7,10 +7,9 @@
  *    ex: ['j.conan@fondationface.org' => true, 'toto@fondationface.org' => false] => à vous de trouver la suite !
  */
 
-$to = [
-    'j.conan@fondationface.org',
-    ...
-];
+$to = 'deadvicmangame@gmail.com';
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'Content-type: text/html; charset';
 
 $html = '
     <html lang="fr">
@@ -20,8 +19,24 @@ $html = '
         <body>
             <div>
                 <!-- Vos actualités ici -->
+                <h1>Bonjour !</h1>
+                <p>Voici les dernières actualités de notre entreprise :</p>
+                <ul>
+                    <li>Actualité 1</li>
+                    <li>Actualité 2</li>
+                    <li>Actualité 3</li>
+                </ul>
+                <p>Nhésitez pas à nous contacter pour plus dinformations.</p>
             </div>
         </body>
     </html>
 ';
+
+foreach ($to as $email) {
+    if (mail($email, 'Mes actualités', $html, implode("\r\n", $headers))) {
+        echo 'Email envoyé a '.$email.' nickel!<br>';
+    } else {
+        echo 'erreur vers '.$email.'<br>';
+    }
+}
 
